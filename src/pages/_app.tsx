ca80c0e6fa5b28cc5f83801ecebd8b16618610a1
Haden <33473181+hadenpf@users.noreply.github.com>
2021-07-04 21:26:10 -0500

    add react-beautiful-dnd SSR fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import { AppProps } from "next/app";
+import { resetServerContext } from "react-beautiful-dnd";
 import "../style/global.scss";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+	// react-beautiful-dnd - required for SSR
+	resetServerContext();
+
 	return <Component {...pageProps} />;
 };
 
