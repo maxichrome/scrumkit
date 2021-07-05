6b43af4d74e8dc3816e8e5b4ebb504d5d52d4ba7
Haden <33473181+hadenpf@users.noreply.github.com>
2021-07-04 21:26:23 -0500

    add ColumnSet to index for testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { ColumnSet } from "../components/columnset";
 
 export default function Home() {
 	return (
 		<div>
 			<p>next app</p>
+			<ColumnSet />
 		</div>
 	);
 }
